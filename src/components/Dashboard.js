a298ac1c2943ddd9ec856a1ca4894096d06b20bb
Andrei <66111825+AndrewDisco@users.noreply.github.com>
2024-04-08 13:37:01 +0300

    Add dash page

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Dashboard.css";
+
+const Dashboard = () => {
+    return(
+        <div><p>caca</p></div>
+        //TODO: Integrate map system using https://docs.mapbox.com/help/tutorials/use-mapbox-gl-js-with-react/
+        // And also get some base functionalities done: see camera feed, idk
+    )
+};
+
+export default Dashboard;
